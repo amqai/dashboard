358c684e2757f473658b1cd3250e9a3b5ccdaf2a
Derrick Hall <vectorl33t2@gmail.com>
2023-07-05 23:37:50 -0400

    Fix routes

--- src/Pages/LoginPage.tsx
+++ src/Pages/LoginPage.tsx
@@ -10,7 +10,7 @@ function LoginPage() {
 
     const submit = async (values: { username: string; password: string }) => {
         const { username, password } = values;
-        const BASE_URL = process.env.REACT_APP_BASE_URL;
+        const BASE_URL = process.env.REACT_APP_API_URL;
         const response = await fetch(`${BASE_URL}/authenticate`, {
             method: 'POST',
             headers: {'Content-Type': 'application/json'},
--- src/Services/ApiService.tsx
+++ src/Services/ApiService.tsx
@@ -1,4 +1,4 @@
-const BASE_URL = process.env.REACT_APP_BASE_URL;
+const BASE_URL = process.env.REACT_APP_API_URL;
 
 export async function fetchProjects(jwt: string) {
     const response = await fetch(`${BASE_URL}/api/person/projects`, {
