a7414883c7b99071c6ec7e7dd01a4553cfebe17c
Derrick Hall <vectorl33t2@gmail.com>
2023-07-31 12:38:57 -0400

    Add pdf upload functionality

--- src/Components/PdfTable.tsx
+++ src/Components/PdfTable.tsx
@@ -1,6 +1,6 @@
-import { Button, Table } from 'antd';
+import { Button, Modal, Space, Spin, Table, Upload } from 'antd';
 import React, { useEffect, useState } from 'react';
-import { DeleteOutlined } from "@ant-design/icons";
+import { DeleteOutlined, UploadOutlined, CheckOutlined } from "@ant-design/icons";
 
 interface PdfTableProps {
     organizationId: string;
@@ -23,7 +23,13 @@ const PdfTable: React.FC<PdfTableProps> = ({
     topicId,
   }) => {
 
+    const DEFAULT_LOADING_MESSAGE = "Uploading file, please wait"
     const [tableData, setTableData] = useState<PdfJob[]>();
+    const [file, setFile] = useState<File | null>(null);
+    const [visible, setVisible] = useState(false);
+    const [activeFile, setActiveFile] = useState<PdfJob | null>(null);
+    const [loading, setLoading] = useState(false);
+    const [loadingMessage, setLoadingMessage] = useState(DEFAULT_LOADING_MESSAGE);
 
     const columns = [
         {
@@ -34,7 +40,18 @@ const PdfTable: React.FC<PdfTableProps> = ({
         {
           key: "2",
           title: "Status",
-          dataIndex: "status",
+          render: (pdfJob: PdfJob) => {
+            return (
+              <>
+                {pdfJob.status === "ACTIVE_EMBEDDING" && (
+                  <Spin>Loading</Spin>
+                )}
+                {pdfJob.status === "FINISH_EMBEDDING" && (
+                  <><CheckOutlined /> Uploaded</>
+                )}
+              </>
+            )
+          }
         },
         {
           key: "3",
@@ -59,7 +76,6 @@ const PdfTable: React.FC<PdfTableProps> = ({
         },
     ];
 
-
     const loadTableData = async() => {
         const jwt = localStorage.getItem('jwt');
         const url = `${import.meta.env.VITE_APP_API_URL}/api/projects/pdf?organizationId=${organizationId}&projectId=${topicId}`;
@@ -79,22 +95,144 @@ const PdfTable: React.FC<PdfTableProps> = ({
         }
     };
 
+    const uploadFile = async () => {
+      if (!file) {
+        return;
+      }
+
+      const formData = new FormData();
+      formData.append('file', file);
+  
+      try {
+        const jwt = localStorage.getItem('jwt');
+        const response = await fetch(`${import.meta.env.VITE_APP_API_URL}/api/projects/pdf?organizationId=${organizationId}&projectId=${topicId}`, {
+          method: 'POST',
+          body: formData,
+          headers: {
+            'Authorization': `Bearer ${jwt}`
+          }
+        });
+  
+        if (response.ok) {
+          const content = await response.json();
+          setActiveFile(content);
+          setLoadingMessage(DEFAULT_LOADING_MESSAGE);
+          setLoading(true);
+          setVisible(true);
+          pollStatus(content.jobId);
+        } else {
+          console.error('Error uploading file:', response.status, response.statusText);
+        }
+      } catch (error) {
+        console.error('Error:', error);
+      }
+    };
+
+    const pollStatus = async (pdfId: string) => {
+      const jwt = localStorage.getItem('jwt');
+      
+      let status = "ACTIVE_INGESTING";
+      let finishedJobResponse = null;
+
+      let count = 1;
+      while (status === "ACTIVE_INGESTING") {
+        console.log("attempt " + count++);
+        const response = await fetch(`${import.meta.env.VITE_APP_API_URL}/api/projects/pdf/${pdfId}?organizationId=${organizationId}&projectId=${topicId}`, {
+          method: 'GET',
+          headers: {
+            'Authorization': `Bearer ${jwt}`
+          }
+        });
+        const content: PdfJob = await response.json();
+        status = content.status;
+        if (status === "FINISH_INGESTING") {
+          finishedJobResponse = content;
+          setLoading(false);
+        } else if (content.status === "ERROR") {
+          console.error("Error finalizing file");
+          return;
+        }
+
+        if (status === "ACTIVE_INGESTING") {
+          await new Promise(resolve => setTimeout(resolve, 1000));
+        }
+      }
+
+      setActiveFile(finishedJobResponse);
+    }
+
+    const handleFileChange = (file: any) => {
+      setFile(file);
+      return false;
+    };
+
+    const handleCancel = () => {
+      setFile(null);
+      setVisible(false);
+    }
+
+    const handleIngesting = async () => {
+      const jwt = localStorage.getItem('jwt');
+      setLoadingMessage("File is being processed, please wait")
+      setLoading(true);
+      await fetch(`${import.meta.env.VITE_APP_API_URL}/api/projects/pdf/${activeFile?.jobId}/embed?organizationId=${organizationId}&projectId=${topicId}`, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${jwt}`
+          }
+      });
+      setFile(null);
+      setLoading(false);
+      setVisible(false);
+      await new Promise(resolve => setTimeout(resolve, 1000));
+      loadTableData();
+    }
+
+    const Loading = () => {
+      if(loading) {
+        return <Space size="middle">{loadingMessage} <br /><Spin size="large" className="spinner" /> </Space>
+      }
+    }
+
     useEffect(() => {
-        (
-            async () => {
-                loadTableData()
-            }
-        )();
+      (
+        async () => {
+          loadTableData()
+        }
+      )();
     }, [organizationId, topicId]);
 
     return (
         <>
             <div className="settings-form-buttons" style={{borderTop: 0}}>
-                <Button type="primary">Upload</Button>
+              <Upload
+                beforeUpload={handleFileChange}
+                accept=".pdf,.json"
+                maxCount={1}
+              >
+                  <Button icon={<UploadOutlined />}>Select File</Button>
+              </Upload>
+            </div>
+            <div  className="settings-form-buttons" style={{borderTop: 0}}>
+              <Button type="primary" onClick={uploadFile}>Upload</Button>
             </div>
             <div className="settings-form-field-100">
                 <Table dataSource={tableData} columns={columns}/>
             </div>
+            <Modal
+              title={"Upload file"}
+              open={visible}
+              onCancel={handleCancel}
+              onOk={handleIngesting}
+              okButtonProps={{ disabled: loading }}
+            >
+              {loading ? (
+                <Loading />
+              ) : (
+                <>Estimated price: ${activeFile?.estimatedPrice}</>
+              )}
+            </Modal>
         </>
     );
   };
