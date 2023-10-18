cd83d623b5a17f4dda58d66fb66fceef43e931a0
Thomas Payne <tepayne97@gmail.com>
2023-10-17 21:45:11 -0700

    route logo to homepage

--- src/Layout/AppHeader.tsx
+++ src/Layout/AppHeader.tsx
@@ -1,10 +1,17 @@
 import { Header } from "antd/es/layout/layout";
+import { useNavigate } from 'react-router-dom';
 
 function AppHeader() {
+    const navigate = useNavigate();
+
+    const navigateHome = () => {
+        navigate(`/`);
+      }
+
     return (
         <Header className="header" style={{display: "flex", justifyContent: "space-between"}}>
             <div>
-                <div className="brand">AMQai</div>
+                <div className="brand" onClick={navigateHome}>AmQai</div>
             </div>
         </Header>
     );
