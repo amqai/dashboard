3b0b2de4bd7302147e386a3a95137f728462efd4
Derrick Hall <vectorl33t2@gmail.com>
2023-10-24 23:49:35 -0400

    Wire up profile page reset password to api

--- src/Pages/Profile/ProfilePage.tsx
+++ src/Pages/Profile/ProfilePage.tsx
@@ -1,50 +1,75 @@
 import { Form, Button, Input, Alert, Typography, Card} from "antd";
 import { useState } from "react";
-
-//TODO: This is you import custom theme
-// const { useToken } = theme;
+import { AlertType, Alert as AlertModel } from "../../models/Alert";
 
 function ProfilePage() {
-    const [errorMessage, setErrorMessage] = useState('');
-    // const { token } = useToken();
+    const [alertMessage, setAlertMessage] = useState<AlertModel | null>(null);
 
-    const resetPassword = async () => {
-        console.log("here")
+    const resetPassword = async (values: { currentPassword: any; newPassword: string }) => {
+        const { currentPassword, newPassword } = values;
+        const jwt = localStorage.getItem('jwt');
+        const response = await fetch(`${import.meta.env.VITE_APP_API_URL}/api/person:password`, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': `Bearer ${jwt}`,
+            },
+            body: JSON.stringify({
+                currentPassword,
+                newPassword,
+            })
+        });
+        
+        if (response.ok) {
+            setAlertMessage({
+                message: 'Your password was successfully updated',
+                type: AlertType.Success,
+            })
+        } else {
+            setAlertMessage({
+                message: 'There was an issue updating your password',
+                type: AlertType.Error,
+            })
+        }
     }
 
     const dismissAlert = () => {
-        setErrorMessage("");
+        setAlertMessage(null);
     };
 
     return (
-    
-        <div className="center-wrapper"
-        style={{
-            // background: token.colorPrimary
-        }}
-        >
+        <div className="center-wrapper">
+            {alertMessage !== null && alertMessage.message !== "" && (
+                <div style={{margin: "24px"}}>
+                <Alert message={alertMessage.message} onClose={dismissAlert} type={alertMessage.type} closable={true} />
+                </div>
+            )}
             <Typography.Title level={3}>Profile</Typography.Title>
             <Card>
                 <h1>Password Reset</h1>
                 <Form className="passwordResetForrm"
                     onFinish={resetPassword}
+                    labelCol={{span: 2}}
                 >
                 <Form.Item
-                    name={"oldPassword"}
+                    name={"currentPassword"}
+                    label="Current Password"
                     rules={[
                         {
                         required: true,
-                        message: "Please enter old password",
+                        message: "Please enter current password",
                         }
                     ]}
                 >                  
-                <Input.Password 
-                    placeholder="Please enter your new password"
-                />
+                    <Input.Password 
+                        placeholder="Please enter your current password"
+                        size="large"
+                    />
                 </Form.Item>
 
                 <Form.Item
                     name={"newPassword"}
+                    label="New Password"
                     rules={[
                         {
                         required: true,
@@ -52,19 +77,16 @@ function ProfilePage() {
                         }
                     ]}
                 >                  
-                <Input.Password placeholder="Please enter your new password" />
+                    <Input.Password 
+                        placeholder="Please enter your new password"
+                        size="large"
+                    />
                 </Form.Item>
 
                 <Button type="primary"
-                htmlType="submit"
-                block
-                style={{width: "auto"}}
+                    htmlType="submit"
+                    size="large"
                 >Update Password</Button>
-                {errorMessage !== "" && (
-                    <div className="erroralert">
-                    <Alert message={errorMessage} onClose={dismissAlert} type="error" closable={true} />
-                    </div>
-                )}
                 </Form>
             </Card>
         </div>
